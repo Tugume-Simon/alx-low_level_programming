d54eaf7e4036115afa839dc46e6a9cef5df9afdf
tugume-peter <simontugume2@gmail.com>
2022-10-13 06:13:03 -0700

    Printing using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * This program prints using the <puts> function
+ * 
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
