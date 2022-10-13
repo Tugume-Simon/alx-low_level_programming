107e299420d808aab337164f145fca522a95c4aa
tugume-peter <simontugume2@gmail.com>
2022-10-13 06:22:20 -0700

    Betty linting 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,13 @@
 /*
  * main - Entry point
  *
- * This program prints using the <puts> function
- * 
+ * Description: This program prints using the <puts> function
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
+	/* This is the main function */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
