14aac021dfa45241b2d2bd28c359e6928188886d
tugume-peter <simontugume2@gmail.com>
2022-10-13 13:56:25 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - everything goes here
+ *
+ * Return: this time 1 (failure)
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58,1,stderr);
+	return (1);
+}
