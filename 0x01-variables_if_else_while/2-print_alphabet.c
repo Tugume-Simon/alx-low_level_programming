1fcce55b47ac802e68c233e38563a69ecf84ae1f
tugume-peter <simontugume2@gmail.com>
2022-10-14 00:45:08 -0700

    the alphabet lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
