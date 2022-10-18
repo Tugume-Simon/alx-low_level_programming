eb4702444b3f362a831674564ef7c30ab6448401
tugume-peter <simontugume2@gmail.com>
2022-10-18 05:46:03 -0700

    printing alphabet from header file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,3 +17,14 @@ void printstr(char *str)
 	}
         putchar('\n');
 }
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
