e622d23b124e60aa9579f77e6e0bad060ab2d917
tugume-peter <simontugume2@gmail.com>
2022-10-18 06:52:54 -0700

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "_putchar.c"
 /**                                                                                                                                                   
  * printstr - function                                                                                                                                
  * @str: string                                                                                                                                       
@@ -7,7 +8,7 @@
  * Description: user defined function for printing a string                                                                                           
  */
 
-/* int _putchar(char c); */
+int _putchar(char c);
 
 void printstr(char *str)
 {
@@ -21,3 +22,7 @@ void printstr(char *str)
         putchar('\n');
 }
 
+void print_alphabet(void)
+{
+	_putchar('a');
+}
