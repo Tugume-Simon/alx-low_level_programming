0df554351691e0cc9cd8a4c46cd3211659874b4e
tugume-peter <simontugume2@gmail.com>
2022-10-18 23:24:44 -0700

    alphabet (lowercase) 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function
+ * Return: no return (void)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
