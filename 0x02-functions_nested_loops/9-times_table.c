e0a061de42d5886879d54f135eec007b80dbaa1f
tugume-peter <simontugume2@gmail.com>
2022-10-19 06:12:34 -0700

    times table from 0 to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,18 +6,30 @@
  */
 void times_table(void)
 {
-	int i, x, product;
+	int i, x, product, i1, i2;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (x = 0; x < 10; x++)
 		{
 			product = x * i;
-			_putchar(product + '0');
+
+			if (product > 9)
+			{
+				i1 = product / 10;
+				i2 = product % 10;
+				_putchar(i1 + '0');
+				_putchar(i2 + '0');
+			}
+			else
+			{
+				_putchar(product + '0');
+			}
+
 			if (x != 9)
 			{
-				_putchar(' ');
 				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
