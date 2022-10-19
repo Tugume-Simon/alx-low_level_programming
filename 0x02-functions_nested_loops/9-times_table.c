038bd5ee5b64532d01d6bedeaedbfa9af7e3320b
tugume-peter <simontugume2@gmail.com>
2022-10-19 05:54:30 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * times_table - printing multiplication tables up to 9
+ * Return: no return value (void)
+ */
+void times_table(void)
+{
+	int i, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			_putchar(x);
+			if (x != 9)
+				_putcahr(' ');
+		}
+		_putchar('\n');
+	}
+}
