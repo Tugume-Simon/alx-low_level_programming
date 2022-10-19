332c4cf3f0317e447341372316f7631aeb11570e
tugume-peter <simontugume2@gmail.com>
2022-10-18 23:38:31 -0700

    alphabet (lowercase) 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,16 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j = 0;
 
-	for (i = 0; i < 10; i++)
+	while (j < 10)
 	{
-		print_alphabet();
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		j++;
 	}
 	_putchar('\n');
 }
