07ff30dfbdbfb01f1f61878c34539ce8eb757fb1
tugume-peter <simontugume2@gmail.com>
2022-10-19 05:32:16 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,29 +6,32 @@
  */
 void jack_bauer(void)
 {
-	int i, n, k, l;
+	int i, n, k, l, flag = 1;
 
 	for (i = 0; i <= 2; i++)
 	{
 		for (n = 0; n < 10; n++)
 		{
+			if (flag == 0)
+				break;
 			for (k = 0; k < 6; k++)
 			{
+				if (flag == 0)
+					break;
 				for (l = 0; l < 10; l++)
 				{
 					if (i == 2 && n == 4)
 					{
+						flag = 0;
 						break;
 					}
-					else
-					{
-						_putchar(i + '0');
-						_putchar(n + '0');
-						_putchar(' ');
-						_putchar(k + '0');
-						_putchar(l + '0');
-						_putchar('\n');
-					}
+
+					_putchar(i + '0');
+					_putchar(n + '0');
+					_putchar(' ');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
 				}
 			}
 		}
