780a18805d02832a59e22935df77facfbc7fff47
tugume-peter <simontugume2@gmail.com>
2022-10-20 05:07:45 -0700

    getting uppercase letters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,14 +10,7 @@ int _isupper(int c)
 {
 	int i;
 
-	if (c >= 65 && c <= 91)
-	{
-		i = 1;
-	}
-	else if (c >= 97 && c <= 122)
-	{
-		i = 0;
-	}
+	i = ((c >= 65 && c <= 91)) ? 1 : 0;
 
 	return (i);
 }
