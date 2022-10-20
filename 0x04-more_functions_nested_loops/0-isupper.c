0f022a2901f9f50729fd214ee1cd113317f418fc
tugume-peter <simontugume2@gmail.com>
2022-10-20 04:55:07 -0700

    is upper?

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase letters.
+ * @c: integer for character
+ * Return: integer, 1 if uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	if (c >= 65 && c <= 91)
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+	}
+
+	return (i);
+}
