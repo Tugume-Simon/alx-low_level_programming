a5896b19a89a4a66c2801473ccfe4e96a5b10e98
tugume-peter <simontugume2@gmail.com>
2022-10-20 11:28:34 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdlib.h>
+
+/**
+ * main - program entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			fprintf(stdout, "Fizz ");
+		else if (i % 5 == 0)
+			fprintf(stdout, "Buzz ");
+		else
+			fprintf(stdout, "%d ", i);
+	}
+	fprintf(stdout, "\n");
+
+	return (0);
+}
