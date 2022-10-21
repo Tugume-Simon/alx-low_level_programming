3493ac98b4a666a37a7020155a8947d04e7f87d5
tugume-peter <simontugume2@gmail.com>
2022-10-21 01:21:35 -0700

    leap year -- print

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include "3-convert_day.c"
-#include "3-print_remaining_days.c"
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
