6f48f602ebbb56269b38008f7179ba10a4cff1c7
tugume-peter <simontugume2@gmail.com>
2022-10-24 13:48:16 -0700

    half of the string print

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@
 
 void puts_half(char *str)
 {
-	int n = 1;
+	int n = 0;
 	int i = 0;
 	int start;
 
@@ -21,7 +21,7 @@ void puts_half(char *str)
 
 	if (n % 2 != 0)
 	{
-		start = (n - 1) / 2;
+		start = 1 + (n - 1) / 2;
 	}
 	else
 	{
