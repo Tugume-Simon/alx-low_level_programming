9cd32549ecb2a04b5348c2222aceacb96fbe07bb
tugume-peter <simontugume2@gmail.com>
2022-10-24 07:01:23 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: string pointer
+ *
+ * Return: count of string characters
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+	while (*s++ != '\0')
+	{
+		l++;
+	}
+
+	return (l);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif /* _MAIN_H */
