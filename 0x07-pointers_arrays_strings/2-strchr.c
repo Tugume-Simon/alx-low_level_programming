ec696aa9ca5506a3228ace589f9f1595e99b1c90
tugume-peter <simontugume2@gmail.com>
2022-10-31 23:21:04 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - searches for a character in a string
+ * @s: the string in which to search
+ * @c: the query character
+ *
+ * Return: pointer to first occurrence of 'c' in 's'
+ * NULL if no match found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
+	return (s);
+}
