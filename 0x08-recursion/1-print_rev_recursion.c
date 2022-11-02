adb63ef85dc07bf34428a9cf90cd5f8e69b1abb9
tugume-peter <simontugume2@gmail.com>
2022-11-02 11:28:39 -0700

    print string in reverse recursively

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (!*s)
+	if (*s != '\0')
 	{
-		_putchar('\0');
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	s--;
-	_print_rev_recursion(s);
 }
