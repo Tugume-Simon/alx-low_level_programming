20be051bada329fb0771241dc5e060ac268ef510
tugume-peter <simontugume2@gmail.com>
2022-11-08 04:37:05 -0800

    copy string from argument and return pointer to it

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - reserves memory to a given string
+ * and copies it to that space
+ * @str: the string
+ *
+ * Return: pointer to newly allocated space in memory
+ * containing a copy of the string provided.
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int s;
+	char *g;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	g = (char *)malloc(sizeof(char) * i);
+	if (g == NULL)
+	{
+		return (NULL);
+	}
+
+	for (s = 0; s < i; s++)
+	{
+		g[s] = str[s];
+	}
+
+	return (g);
+}
