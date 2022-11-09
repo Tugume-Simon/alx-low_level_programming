598beb3fe98ae286c43a408e79afacf686dc787b
tugume-peter <simontugume2@gmail.com>
2022-11-08 18:44:30 -0800

    joining arg strings with newline

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,7 +29,7 @@ char *argstostr(int ac, char **av)
 	}
 
 	c--;
-	rstr = (char *)malloc(sizeof(char) * (c - n));
+	rstr = (char *)malloc(sizeof(char) * (c - n - 1));
 	if (rstr == NULL)
 		return (NULL);
 	c = 0;
