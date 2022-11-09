1167763871cdffcaa3d7195e18aa27578b121e3b
tugume-peter <simontugume2@gmail.com>
2022-11-08 18:15:19 -0800

    joining arg strings with newline

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - joins strings from provided
+ * arguments by newline
+ * @ac: number of arguments
+ * @av: pointer to string arguments
+ *
+ * Return: pointer to concatenated string
+ */
+char *argstostr(int ac, char **av)
+{
+	int n, k, c;
+	char *rstr;
+	char g;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (n = 1; n < ac; n++)
+	{
+		k = 0;
+		while (av[n][k])
+		{
+			c++;
+			k++;
+		}
+	}
+
+	rstr = (char *)malloc(sizeof(char) * (c + ac)); /* c + ac - 1 + 1 */
+	if (rstr == NULL)
+		return (NULL);
+	c = 0;
+	for (n = 1; n < ac; n++)
+	{
+		k = 0;
+		while ((g = av[n][k]))
+		{
+			rstr[c] = g;
+			c++;
+			k++;
+		}
+		rstr[c] = '\n';
+		c++;
+	}
+
+	return (rstr);
+}
