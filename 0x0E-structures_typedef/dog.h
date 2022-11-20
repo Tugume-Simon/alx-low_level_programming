63fc49dc5c1be32db73896bb8ea9f3acdf7f52df
tugume-peter <simontugume2@gmail.com>
2022-11-19 21:00:14 -0800

    defining new structure dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - structure to hold dog information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of the dog
+ *
+ * Description: this template will be used to create
+ * other dog structs
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /* _DOG_H */
