cc2e8d8fb4cf143cb928c71d49ad3bcb6968896f
tugume-peter <simontugume2@gmail.com>
2022-11-20 05:22:31 -0800

    create a new dog_t struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,54 +3,6 @@
 #include <stdio.h>
 
 /**
- * get_length - finds the length of a string
- * @str: pointer to the string
- *
- * Return: number of characters in the string
- */
-/*
-int get_length(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-*/
-/**
- * cp_str - makes a copy of a string
- * @s: pointer to the string
- * @len: length of the string
- *
- * Return: copy of string addressed by s
- */
-/*
-char *cp_str(char *s, int len)
-{
-	int i;
-	char *str;
-
-	str = malloc(sizeof(char) * len + 1);
-	if (str == NULL)
-	{
-		free(str);
-		return (NULL);
-	}
-
-	i = 0;
-	while (i < len)
-	{
-		str[i] = s[i];
-		i++;
-	}
-	return (str);
-}
-*/
-/**
  * new_dog - creates a new dog type (struct)
  * @name: name of the dog
  * @age: dog's age
@@ -61,8 +13,11 @@ char *cp_str(char *s, int len)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
-	char *cpname;
-	char *cpowner;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
@@ -71,24 +26,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	cpname = malloc(sizeof(*name));
-	if (cpname == NULL)
-	{
-		free(cpname);
-		return (NULL);
-	}
-	cpname = name;
-	cpowner = malloc(sizeof(*owner));
-	if (cpowner == NULL)
-	{
-		free(cpowner);
-		return (NULL);
-	}
-	cpowner = owner;
+	new->name = malloc(sizeof(*name));
+	new->owner = malloc(sizeof(*owner));
 
-	new->name = cpname;
-	new->owner = cpowner;
+	new->name = name;
 	new->age = age;
+	new->owner = owner;
 
 	return (new);
 }
