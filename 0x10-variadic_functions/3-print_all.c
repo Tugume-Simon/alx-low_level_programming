4f3b47f7705f5ee0ed5164f08b5fa87c5fd7329f
tugume-peter <simontugume2@gmail.com>
2022-11-24 00:53:52 -0800

    printing all arguments with format

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of types supplied
+ *
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	char f, l;
+	char *s;
+	int i, j;
+	double fl;
+	va_list ap;
+
+	j = 0;
+	va_start(ap, format);
+	while ((f = format[j]))
+	{
+		switch (f)
+		{
+			case 'c':
+				l = va_arg(ap, int);
+				_putchar(l);
+				break;
+			case 'i':
+				i = va_arg(ap, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				fl = va_arg(ap, double);
+				printf("%0.6f", fl);
+				break;
+			case 's':
+				s = va_arg(ap, char*);
+				printf("%s", s);
+				break;
+			default:
+				j++;
+				continue;
+		}
+		if (format[j + 1])
+			printf(", ");
+		j++;
+	}
+	printf("\n");
+
+	va_end(ap);
+}
