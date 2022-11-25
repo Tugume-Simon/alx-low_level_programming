fc98694b69e22729697013596a613409ed25070b
tugume-peter <simontugume2@gmail.com>
2022-11-24 23:20:18 -0800

    adding new node at end of linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,6 +16,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *tmp;
 
 	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = NULL;
