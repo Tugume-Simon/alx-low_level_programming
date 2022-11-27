b6528fd15e5064c7315b2e579f20fa7824a473a2
tugume-peter <simontugume2@gmail.com>
2022-11-26 19:56:35 -0800

    creating new dog struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,45 +3,6 @@
 #include <stdio.h>
 
 /**
- * get_length - counts characters in a string
- * @s: pointer to string
- *
- * Return: length of string
- */
-int get_length(char *s)
-{
-	int i;
-
-	if (!s)
-		return (0);
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * copy_str - copies string from one memory address to another
- * @src: memory address to copy fromm
- * @dest: memory address to copy to
- *
- * Return: nothing
- */
-void copy_str(char *src, char *dest)
-{
-	int i;
-
-	i = 0;
-	while(src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-}
-
-/**
  * new_dog - creates a new dog type (struct)
  * @name: name of the dog
  * @age: dog's age
@@ -52,8 +13,6 @@ void copy_str(char *src, char *dest)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
-	char *nm;
-	char *own;
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
@@ -62,13 +21,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	nm = malloc(sizeof(char) * get_length(name));
-	own = malloc(sizeof(char) * get_length(owner));
-
-	copy_str(name, nm);
-	copy_str(name, own);
-	new->name = nm;
-	new->owner = own;
+	new->name = name;
+	new->owner = owner;
 	new->age = age;
 
 	return (new);
