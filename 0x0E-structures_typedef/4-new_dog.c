0807a35f8dee47a7d931533af33881d0ee0e186b
tugume-peter <simontugume2@gmail.com>
2022-11-27 03:19:22 -0800

    new dog from dog_t struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,6 +27,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new->name);
 		free(new->owner);
 		free(new);
+		new->name = NULL;
+		new->age = NULL;
+		new->owner = NULL;
 		return (new);
 	}
 	for (i = 0; i < n; i++)
