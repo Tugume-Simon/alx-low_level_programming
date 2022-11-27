d246d7e07e4a0b12fe5fe4b263ac44f830e86fe1
tugume-peter <simontugume2@gmail.com>
2022-11-27 01:45:01 -0800

    new dog from dog_t struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,12 +30,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		free(new->name);
 		free(new);
+		return (NULL);
 	}
 	new->owner = malloc(sizeof(char) * o);
 	if (!new->owner)
 	{
 		free(new->owner);
 		free (new);
+		return (NULL);
 	}
 	for (i = 0; i < n; i++)
 	{
