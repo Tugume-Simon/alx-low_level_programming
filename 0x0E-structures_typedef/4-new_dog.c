98d05daf053ff6573f6546f1ea27e905b1e643b0
tugume-peter <simontugume2@gmail.com>
2022-11-26 21:31:53 -0800

    new dog from dog_t

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -39,9 +39,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		own[i] = owner[i];
 	}
 
-	new->name = nm;
+	new->name = name;
 	new->age = age;
-	new->owner = own;
+	new->owner = owner;
 
 	return (new);
 }
