a68151b05e995f44828143b8d9ea52a400cc81ff
tugume-peter <simontugume2@gmail.com>
2022-11-26 21:30:30 -0800

    new dog from dog_t

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,9 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+int get_len(char *str);
 /**
  * new_dog - creates a new dog type (struct)
  * @name: name of the dog
@@ -13,7 +15,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
-
+	char *nm, *own;
+	int i, nl, ol;
+	
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 	{
@@ -21,9 +25,45 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new->name = name;
-	new->owner = owner;
+	nl = get_len(name);
+	ol = get_len(owner);
+	nm = malloc(sizeof(char) * nl);
+	own = malloc(sizeof(char) * ol);
+
+	for (i = 0; i < nl; i++)
+	{
+		nm[i] = name[i];
+	}
+	for (i = 0; i < ol; i++)
+	{
+		own[i] = owner[i];
+	}
+
+	new->name = nm;
 	new->age = age;
+	new->owner = own;
 
 	return (new);
+}
+
+/**
+ * get_len - counts characters in the string
+ * @str: pointer to string
+ *
+ * Return: length of string
+ */
+int get_len(char *str)
+{
+	int i;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
