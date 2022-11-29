704e4355dc952d586565ffbb4b8cff543627edb6
tugume-peter <simontugume2@gmail.com>
2022-11-28 18:19:24 -0800

    free listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees memory held by singly linked list
+ * @head: pointer to head node of singly linked list
+ *
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *one;
+
+	if (head == NULL)
+	{
+		exit(0);
+	}
+
+	one = head->next;
+	free(head);
+	free_listint(one);
+}
