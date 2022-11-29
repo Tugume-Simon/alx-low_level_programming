cb59c5b154193646244ff8575254324dd4070322
tugume-peter <simontugume2@gmail.com>
2022-11-28 18:02:17 -0800

    adding nodes at either end of singly linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
+	listint_t *tmp;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
@@ -28,7 +29,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (new);
 	}
 
-	new->next = *head;
-	*head = new;
+	tmp = *head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+
+	tmp->next = new;
 	return (new);
 }
