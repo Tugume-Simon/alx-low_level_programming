b3626f8b74be478977a8172f4ce02e42a7101181
tugume-peter <simontugume2@gmail.com>
2022-12-05 22:31:23 -0800

    reading text from a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,16 +17,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read;
 	ssize_t written;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (filename == NULL || fd == -1)
 	{
 		return (0);
 	}
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		return (0);
-	}
 	buff = malloc(sizeof(char) * letters);
 	if (!buff)
 		return (0);
