9178383a494091719f77ee5ace805ad62616a5fd
tugume-peter <simontugume2@gmail.com>
2022-12-06 19:47:05 -0800

    copy text of one file into another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - copies text content of one file to another
+ * @argc: number of commandline arguments
+ * @argv: commandline arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fdes1, fdes2;
+	char buff[1024];
+	ssize_t file_read;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fdes1 = open(argv[1], O_RDONLY);
+	if (fdes1 == -1)
+	{
+		dprintf(2, "Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fdes2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (fdes2 == -1)
+	{
+		dprintf(2, "Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((file_read = read(fdes1, buff, sizeof(buff))))
+	{
+		if (!write(fdes2, buff, file_read))
+		{
+			dprintf(2, "Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+		lseek(fdes1, file_read, SEEK_CUR);
+	}
+
+	if (close(fdes2) == -1)
+	{
+		dprintf(2, "Can't close fd %d\n", fdes2);
+		exit(100);
+	}
+	if (close(fdes1) == -1)
+	{
+		dprintf(2, "Can't close fd %d\n", fdes1);
+		exit(100);
+	}
+	return (0);
+}
