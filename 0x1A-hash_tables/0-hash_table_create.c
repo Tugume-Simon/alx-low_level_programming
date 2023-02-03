98ef3b0c66a52930af93d564e6221658c96daef2
tugume-peter <simontugume2@gmail.com>
2023-02-02 21:04:23 -0800

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ * Or NULL on failue
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t*) * size);
+
+	if (table->array == NULL)
+		return (NULL);
+
+	/*
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	*/
+
+	return (table);
+}
