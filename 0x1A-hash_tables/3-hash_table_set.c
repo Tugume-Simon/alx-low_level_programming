26d69840d7f049609db6e05d46c85df2add6d6fa
tugume-peter <simontugume2@gmail.com>
2023-02-02 23:01:15 -0800

    insert node in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,6 +16,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	hash_node_t *tmp;
 
+	if (key == NULL || strcmp(key, "") == 0)
+		return (0);
+
 	index = key_index((unsigned char *)key, ht->size);
 	node = malloc(sizeof(hash_node_t));
 	node->key = malloc(sizeof(key) + 1);
