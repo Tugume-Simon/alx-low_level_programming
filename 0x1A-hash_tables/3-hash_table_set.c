6e53a27e4173356f91503f6ad228f834a11b0930
tugume-peter <simontugume2@gmail.com>
2023-02-03 00:59:39 -0800

    add element in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+int is_empty(const char *str);
+char *_strcpy(char *dest, char *src);
 /**
  * hash_table_set - adds an element to a hash table
  * @ht: pointer to the hash table
@@ -16,13 +18,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	hash_node_t *tmp;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
-	if (key == NULL || value == NULL)
-		return (0);
-
-	if (strcmp(key, "") == 0)
+	if (is_empty(key))
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
@@ -35,8 +34,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	strcpy(node->key, key);
-	strcpy(node->value, value);
+	_strcpy(node->key, (char *)key);
+	_strcpy(node->value, (char *)value);
 	node->next = NULL;
 
 	if ((ht->array)[index] == NULL)
@@ -51,3 +50,50 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	return (1);
 }
 
+/**
+ * is_empty - checks whether a given string is an empty string
+ * @str: string
+ *
+ * Return: 1 on success, 0 on failue
+ */
+int is_empty(const char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str++)
+	{
+		i++;
+	}
+
+	if (i > 0)
+		return (0);
+
+	return (1);
+}
+
+/**
+ * _strcpy - copies characters from one location to another
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char c;
+	int i;
+
+	if (dest == NULL || src == NULL)
+		return (NULL);
+
+	i = 0;
+	while ((c = *src) != '\0')
+	{
+		dest[i] = *src;
+		src++;
+		i++;
+	}
+
+	return (dest);
+}
